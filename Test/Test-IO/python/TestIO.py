9b4c41f4ae236d7a348e4e5a436120fcbc2fe6e2
Pierre Schönbeck <ic.pierre.gc@gmail.com>
2015-04-13 17:38:02 +0200

    Added python program for testIO

---
+++ Test/Test-IO/python/TestIO.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import timeit, sys
+
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped
+
+def start(file, outfile):
+    input = open(file, 'r')
+    output = open(outfile, 'w')
+
+    line = input.readline()
+    while line:
+        line = line.replace('Tellus', 'Terra')
+        line = line.replace('tellus', 'terra')
+        output.write(line)
+        line = input.readline()
+
+def main(argv):
+    file = 'dump.txt'
+    output = 'res.txt'
+    for i in range(len(argv)):
+        if argv[i] == '-f':
+            i = i + 1
+            file = argv[i]
+        elif argv[i] == '-o':
+            i = i + 1
+            output = argv[i]
+
+    #ns = time.time()
+    wrapped = wrapper(start, file, output)
+    print (timeit.timeit(wrapped, number=1))
+    #totaltime = (time.time() - ns) / 1000000
+    #print (totaltime)
+    sys.exit(0)
+
+if __name__ == '__main__':main([])
