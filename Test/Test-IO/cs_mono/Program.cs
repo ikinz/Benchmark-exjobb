91ca83af62afa7a262d54dcc89018d86a9c84aaa
Pierre Schönbeck <ic.pierre.gc@gmail.com>
2015-04-13 18:13:28 +0200

    Added c#-mono program for testIO

---
+++ Test/Test-IO/cs_mono/Program.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace TestIO
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string file = "dump.txt";
+			string output = "res.txt";
+			for (int i = 0; i < args.Length; i++) {
+				if (args [i].Equals ("-f")) {
+					file = args [++i];
+				} else if (args [i].Equals ("-o")) {
+					output = args [++i];
+				}
+			}
+
+			Stopwatch sw = new Stopwatch ();
+			sw.Start();
+			start (file, output);
+			sw.Stop ();
+			TimeSpan et = sw.Elapsed;
+			Console.WriteLine (et.TotalMilliseconds);
+		}
+
+		public static void start(string file, string output) {
+			if (System.IO.File.Exists (output)) {
+				System.IO.File.Delete (output);
+			}
+
+			StreamReader sr = new StreamReader (file);
+			StreamWriter sw = new StreamWriter (output);
+
+			try {
+				String line;
+				while ((line = sr.ReadLine()) != null) {
+					line = line.Replace("Tellus", "Terra");
+					line = line.Replace("tellus", "terra");
+
+					sw.WriteLine(line);
+				}
+			} catch (Exception e) {
+
+			} finally {
+				sr.Close ();
+				sw.Close ();
+			}
+		}
+	}
+}
