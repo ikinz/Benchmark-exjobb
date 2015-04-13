449639a3539a73feb3f87a55dab8b5ecd368d4ef
Pierre Schönbeck <ic.pierre.gc@gmail.com>
2015-04-13 15:25:25 +0200

    TestIO java done

--- Test/Test-IO/java/TestIO.java
+++ Test/Test-IO/java/TestIO.java
@@ -1,3 +1,4 @@
+import java.io.*;
 
 public class TestIO {
 	public static void main(String[] args) {
@@ -10,13 +11,33 @@ public class TestIO {
 				output = args[++i];
 			}
 		}
-		
+
 		long ns = System.nanoTime();
-		new TestIO().start();
-		System.out.println(System.nanoTime() - ns);
+		new TestIO().start(file, output);
+		long time = (System.nanoTime() - ns)/1000000;
+		System.out.println(time + "ms");
 	}
-	
-	public void start() {
-		
+
+	public void start(String file, String output) {
+		try {
+			BufferedReader br = new BufferedReader
+				(new InputStreamReader(new FileInputStream(file)));
+			BufferedWriter bw = new BufferedWriter
+				(new OutputStreamWriter(new FileOutputStream(output)));
+
+			String line;
+
+			while ((line = br.readLine()) != null) {
+				line = line.replace("Tellus", "Terra");
+				line = line.replace("tellus", "Terra");
+
+				bw.append(line + "\n");
+			}
+
+			br.close();
+			bw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
