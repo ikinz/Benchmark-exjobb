33c4510878a8260da4f6d34bb37830cf63ffcb2c
Pierre Schönbeck <ic.pierre.gc@gmail.com>
2015-04-20 00:07:20 +0200

    Fixed TestIO.java

--- Test/Test-IO/java/TestIO.java
+++ Test/Test-IO/java/TestIO.java
@@ -1,4 +1,8 @@
-import java.io.*;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class TestIO {
 	public static void main(String[] args) {
@@ -19,6 +23,33 @@ public class TestIO {
 	}
 
 	public void start(String file, String output) {
+		/*
+		String txt = "";
+		try {
+			File f = new File(file);
+			FileInputStream inp = new FileInputStream(f);
+			byte[] bf = new byte[(int)f.length()];
+			inp.read(bf);
+			txt = new String(bf, "UTF-8");
+		} catch (Exception e) {
+			
+		}
+		*/
+		try {
+			String txt = ReadAll.readAllLines(file);
+			String[] arr = txt.split("\n");
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = arr[i].replace("Tellus", "Terra");
+				arr[i] = arr[i].replace("tellus", "terra");
+			}
+			String.join("\n", arr);
+			ReadAll.writeAllLines(output, );
+		} catch (Exception e) {
+			
+		}
+		
+		
+		/*
 		try {
 			BufferedReader br = new BufferedReader
 				(new InputStreamReader(new FileInputStream(file)));
@@ -38,6 +69,22 @@ public class TestIO {
 			bw.close();
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+		*/
+	}
+	
+	private static class ReadAll {
+		public static String readAllLines(String filename) throws IOException {
+			Path file = Paths.get(filename);
+			StringBuilder res = new StringBuilder();
+			for (String s : Files.readAllLines(file, Charset.defaultCharset()))
+				res.append(s);
+			
+			return res.toString();
+		}
+		public static void writeAllLines(String file, String content) throws IOException {
+			Path f = Paths.get(file);
+			Files.write(f, content.getBytes());
 		}
 	}
 }
