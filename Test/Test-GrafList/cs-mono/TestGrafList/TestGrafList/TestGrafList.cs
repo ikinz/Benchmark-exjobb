d8599b5f646dcd52bce3e143403fe08001a0ecf0
Pierre Schönbeck <ic.pierre.gc@gmail.com>
2015-05-20 14:50:24 +0200

    added graf test mono

---
+++ Test/Test-GrafList/cs-mono/TestGrafList/TestGrafList/TestGrafList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+using Gtk;
+
+namespace TestGrafList
+{
+	public class TestGrafList
+	{
+		public static void Main(string[] args)
+		{
+			Application.Init ();
+
+			string file = "dump.txt";
+			string output = "res.txt";
+			for (int i = 0; i < args.Length; i++) {
+				if (args [i].Equals ("-f")) {
+					file = args [++i];
+				} else if (args [i].Equals ("-o")) {
+					output = args [++i];
+				}
+			}
+
+
+			Stopwatch sw = new Stopwatch ();
+			sw.Start();
+			new TestGrafList ().start (file, output);
+			sw.Stop ();
+			TimeSpan et = sw.Elapsed;
+			Console.WriteLine (et.TotalMilliseconds);
+
+			//return;
+			Application.Run ();
+		}
+
+		public void start(string file, string output) 
+		{
+			win_one (file, output);
+		}
+
+		private void win_one(string file, string output) {
+			Win_one frame = new Win_one ();
+			frame.DeleteEvent += delete_event;
+			frame.Show ();
+
+			if (System.IO.File.Exists (output)) {
+				System.IO.File.Delete (output);
+			}
+
+			StreamReader sr = new StreamReader (file);
+			StreamWriter sw = new StreamWriter (output);
+
+			try {
+				string line;
+				int counter = 0;
+				while ((line = sr.ReadLine()) != null) {
+					frame.AddItem(++counter, line);
+				}
+
+				frame.mvItems ("tellus");
+			} catch (Exception e) {
+				Console.WriteLine (e.ToString());
+			} finally {
+				sr.Close ();
+				sw.Close ();
+			}
+		}
+
+		static void delete_event (object obj, DeleteEventArgs args)
+		{
+			Application.Quit ();
+		}
+	}
+}
+
---
+++ Test/Test-GrafList/cs-mono/TestGrafList/TestGrafList/Win_one.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestGrafList
+{
+	public partial class Win_one : Gtk.Window
+	{
+		private Gtk.ListStore store1;
+		private Gtk.ListStore store2;
+
+		public Win_one () :
+			base (Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+
+			store1 = new Gtk.ListStore (typeof(int), typeof(string));
+			store2 = new Gtk.ListStore (typeof(int), typeof(string));
+
+			treeview1.AppendColumn ("Row", new Gtk.CellRendererText (), "text", 0);
+			treeview1.AppendColumn ("String", new Gtk.CellRendererText (), "text", 1);
+
+			treeview2.AppendColumn ("Row", new Gtk.CellRendererText (), "text", 0);
+			treeview2.AppendColumn ("String", new Gtk.CellRendererText (), "text", 1);
+
+			treeview1.Model = store1;
+			treeview2.Model = store2;
+		}
+
+		public void AddItem(int row, string item) {
+			store1.AppendValues (row, item);
+		}
+
+		public void mvItems (string search) {
+			Gtk.TreeIter iter;
+			store1.GetIterFirst (out iter);
+
+			for (int i = 0; i < store1.IterNChildren (); i++) {
+				int row = Convert.ToInt32(store1.GetValue (iter, 0));
+				string str = store1.GetValue (iter, 1) as string;
+
+				if (str.Contains(search)) {
+					store2.AppendValues (row, str);
+				}
+
+
+				store1.IterNext (ref iter);
+			}
+
+			/*store1.Foreach( (Gtk.TreeModel model, Gtk.TreePath path, Gtk.TreeIter iter) => {
+				string val = model.GetValue(iter, 1) as string;
+				Console.WriteLine(val);
+
+				if (val.Contains(search)) {
+					int row = Int32.Parse(model.GetValue(iter, 0) as string);
+
+					//Console.WriteLine(row + ": " + val);
+
+					store2.AppendValues(row, val);
+				}
+
+				return true;
+			});*/
+		}
+	}
+}
+
